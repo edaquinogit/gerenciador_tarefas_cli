1c33cb758e3f9ad21a2049e49fa7fa86036d8504
Ednaldo Aquino <ednaldoaquino87@gmail.com>
2025-12-26 12:58:48 -0300

    chore: estrutura inicial do projeto CLI

---
+++ main.py
@@ -0,0 +1,14 @@
+# Programa: Gerenciador de Tarefas Simples - CLI
+# Autor: Ednaldo Aquino
+# Descrição: Projeto em Python para gerenciamento de tarefas via terminal.
+
+print('===================================================')
+print('    GERENCIADOR DE TAREFAS SIMPLES - CLI    ')
+print('===================================================')
+
+nome_usuario = input('Digite seu nome:')
+
+print(f'\nOlá, {nome_usuario}! Bem-vindo ao Sistema.')
+print('Este projeto será desenvolvido passo a passo.')
+
+
