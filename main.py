e2f187618eb656df8c303e50184a8af1d7bd3fbd
Ednaldo Aquino <ednaldoaquino87@gmail.com>
2025-12-27 19:05:45 -0300

    feat: adicionar funcionalidade de remover tarefas

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ def exibir_menu():
     Exibe o menu principal do gerenciador de tarefas.
     '''
     print('\n--- GERENCIADOR DE TAREFAS ---')
-    print('1. Adicionar tarefa')
+    print('\n1. Adicionar tarefa')
     print('2. Listar tarefas')
-    print('3. Sair')
+    print('3. Remover tarefa')
+    print('4. Sair')
 
 
 def adicionar_tarefa():
@@ -33,6 +34,27 @@ def listar_tarefas():
         for indice, tarefa in enumerate(tarefas, start=1):
             print(f'{indice}. {tarefa}')
 
+def remover_tarefa():
+    '''
+    Remove uma tarefa da lista.
+    '''
+    if len(tarefas) == 0:
+        print('nenhuma tarefa cadrastrada para remover.')
+        return
+
+    listar_tarefas()
+
+    try:
+        numero = int(input(('Digite o número da tarefa que deseja remover:')))
+        indice = numero - 1
+        if 0 <= indice < len(tarefas):
+            tarefas_removida = tarefas.pop(indice)
+            print(f'Tarefa removida: {tarefas_removida}')
+        else:
+            print('Número invalido.')
+    except ValueError:
+        print('Por favor, digite um numero valido.')
+        
 def main():
     while True:
         exibir_menu()
@@ -43,6 +65,8 @@ def main():
         elif opcao == '2':
             listar_tarefas()
         elif opcao == '3':
+            remover_tarefa()
+        elif opcao == '4':
             print('Saindo do programa. Até logo!')
             break
         else:
