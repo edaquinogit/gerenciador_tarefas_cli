c3d8f6b0e661c944afba789d5d03174cea1d6530
Ednaldo <a_ednaldo@icloud.com>
2026-01-04 04:32:54 -0300

    delete tasks

--- src/api/tasks.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from src.core.models import Tarefa
-
-router = APIRouter()  # <--- Esta linha é a que está a faltar! 🚀
-
-@router.post("/tarefas")
-def criar_tarefa(nova_tarefa: Tarefa):
-    return {"mensagem": "Tarefa criada!", "dados": nova_tarefa}
